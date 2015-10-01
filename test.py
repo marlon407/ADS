48227126b3c3d3c35ee7c8ffaedff16597f73c41
Marlon Casagranda <marlondiego407@gmail.com>
2015-10-01 19:18:02 -0300

    Updated test
    Now with two servers in the environment, each of them with a different server time

--- test.py
+++ test.py
@@ -1,33 +1,97 @@
+"""
+Carwash example.
+
+Covers:
+
+- Waiting for other processes
+- Resources: Resource
+
+Scenario:
+  A carwash has a limited number of washing machines and defines
+  a washing processes that takes some (random) time.
+
+  Car processes arrive at the carwash at a random time. If one washing
+  machine is available, they start the washing process and wait for it
+  to finish. If not, they wait until they an use one.
+
+"""
 import random
 import simpy
 import itertools
 
+RANDOM_SEED = 42
+NUM_SERVER = 2  # Number of machines in the carwash
+SIM_TIME = 200     # Simulation time in minutes
+servidorFree = 1
 list = [3,3,3,6,6, 12,12, 30, 22,44]
+#list = [4, 4, 4]
 servidor1 = [12, 12, 11, 11, 10.8, 10, 12.2, 13]
 servidor2 = [12, 12.2, 13,13.5, 11,2, 10.8, 10]
 
-def processo(name, env):
-    """A car arrives at the gas station for refueling.
 
-    It requests one of the gas station's fuel pumps and tries to get the
-    desired amount of gas from it. If the stations reservoir is
-    depleted, the car has to wait for the tank truck to arrive.
+class Server(object):
+    def __init__(self, env, num_servers):
+        self.env = env
+        self.machine = simpy.Resource(env, num_servers)
+        self.serverTime = 1
+
+    def do(self, process):
+        yield self.env.timeout(self.serverTime)
+        print("procss %s left." %process)
+
+
+def processo(env, name, cw):
+    """The car process (each car has a ``name``) arrives at the carwash
+    (``cw``) and requests a cleaning machine.
+
+    It then starts the washing process, waits for it to finish and
+    leaves to never come back ...
 
     """
-    print('%s arriving at the server at %.1f' % (name, env.now))
-    yield env.timeout(random.choice(servidor1))
-    print('%s leaving at the server at %.1f' % (name, env.now))
+    print('%s arrives at the server at %.2f.' % (name, env.now))
+    with cw.machine.request() as request:
+        yield request
+        global servidorFree
 
-def process_generator(env):
-    """Generate new cars that arrive at the gas station."""
-    for i in itertools.count():
+        if servidorFree:
+            print("%s vou usar o servidor 1" % name)
+            cw.serverTime = random.choice(servidor1)
+            servidorFree = 0
+        else:
+            print("%s vou usar o servidor 2" % name)
+            cw.serverTime = random.choice(servidor2)
+            servidorFree = 1
+
+
+
+        print('%s enters the server at %.2f.' % (name, env.now))
+        yield env.process(cw.do(name))
+        print('%s leaves the server at %.2f.' % (name, env.now))
+
+
+def setup(env, nun_server):
+    """Create a carwash, a number of initial cars and keep creating cars
+    approx. every ``t_inter`` minutes."""
+    # Create the environment
+    ambiente = Server(env, nun_server)
+    i = 0
+    # Create more cars while the simulation is running
+    servidor0Free = 1
+    while True:
         yield env.timeout(random.choice(list))
-        print('print')
-        env.process(processo('Car %d' % i, env))
+        env.process(processo(env, 'process %d' % i, ambiente))
+        i += 1
 
-# Create environment and start processes
+    
+    
+
+
+# Setup and start the simulation
+random.seed(RANDOM_SEED)  # This helps reproducing the results
+
+# Create an environment and start the setup process
 env = simpy.Environment()
-env.process(process_generator(env))
+env.process(setup(env, NUM_SERVER))
 
 # Execute!
-env.run(until=100)
+env.run(until=SIM_TIME)
